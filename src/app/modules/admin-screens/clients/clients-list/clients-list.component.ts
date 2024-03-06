e1c8a8435a1b6a0f151fd03acdb01fbfe90da940
ChethanPrasadL <chethan.prasad@simbiotiktech.com>
2024-03-06 11:52:54 +0530

    table header

---
+++ src/app/modules/admin-screens/clients/clients-list/clients-list.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-clients-list',
+  templateUrl: './clients-list.component.html',
+  styleUrls: ['./clients-list.component.scss']
+})
+export class ClientsListComponent {
+    displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+    dataSource = ELEMENT_DATA;
+  }
+
+  export interface PeriodicElement {
+    name: string;
+    position: number;
+    weight: number;
+    symbol: string;
+  }
+
+  const ELEMENT_DATA: PeriodicElement[] = [
+    {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  ];
+
+
---
+++ src/app/modules/admin-screens/clients/clients.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ClientsListComponent } from './clients-list/clients-list.component';
+import { RouterModule, Routes } from '@angular/router';
+import { MaterialModule } from 'src/app/material/material.module';
+
+const routes: Routes = [
+  { path: '', component: ClientsListComponent},
+  
+  ]
+
+@NgModule({
+  declarations: [
+    ClientsListComponent
+  ],
+  imports: [
+    CommonModule,
+    MaterialModule,
+    RouterModule.forChild(routes),
+  ]
+})
+export class ClientsModule { }
