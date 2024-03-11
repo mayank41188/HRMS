cc04bb613824f3812fd5870211e17771ea71c69c
SharathKumar2012 <121597382+SharathKumar2012@users.noreply.github.com>
2024-03-11 10:00:40 +0530

    holiday

---
+++ src/app/hr/holiday/add-holiday-popup/add-holiday-popup.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-holiday-popup',
+  templateUrl: './add-holiday-popup.component.html',
+  styleUrls: ['./add-holiday-popup.component.scss']
+})
+export class AddHolidayPopupComponent {
+
+}
