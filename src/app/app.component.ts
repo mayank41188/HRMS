ec06fad78d490064d8845d3c746baa9e381c935b
kerena7 <kerena@simbiotikgroup.com>
2024-03-28 12:20:28 +0530

    sidebar icons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -8,12 +10,30 @@ import { RouterOutlet } from '@angular/router';
     standalone : true,
     imports    : [RouterOutlet],
 })
-export class AppComponent
-{
-    /**
-     * Constructor
-     */
-    constructor()
+export class AppComponent{
+    icons: Array<string> = [
+        'dashboard',
+        'Employee',
+        'Leave-Management',
+        'Holidays',
+        'Payroll',
+        'Attendance',
+        'Jobs-Recruitment',
+        'Task',
+        'Policy-Documents',
+        'Events',
+        'Reports',
+        'Tax-Forms',
+        'Projects'
+    ];
+    constructor(
+        private iconRegistry: MatIconRegistry,
+        private sanitizer: DomSanitizer
+    )
     {
+        this.icons.map(e => {
+            this.iconRegistry.addSvgIcon(
+                e, this.sanitizer.bypassSecurityTrustResourceUrl(`../assets/icons/sidebar/${e}.svg`));
+        });
     }
 }
